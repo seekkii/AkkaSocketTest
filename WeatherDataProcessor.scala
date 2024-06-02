e54429d20c4a29d5c244b7cad1125785686fe748
seekkii <92387324+seekkii@users.noreply.github.com>
2024-06-03 00:26:01 +0700

    Update WeatherDataProcessor.scala

--- WeatherDataProcessor.scala
+++ WeatherDataProcessor.scala
@@ -1,150 +1,102 @@
-import org.apache.pekko.actor.{ActorRef, ActorSystem}
-import org.apache.pekko.stream.{ActorMaterializer, Materializer}
-import org.apache.pekko.stream.scaladsl.{Framing, Sink, Source, StreamConverters}
+package scala
+
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.stream.Materializer
+import org.apache.pekko.stream.scaladsl.{Framing, Sink, StreamConverters}
 import org.apache.pekko.util.ByteString
 
-import java.io.{BufferedReader, File, FileInputStream}
-import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
-import scala.language.{implicitConversions, postfixOps}
 import java.io.{File, FileInputStream}
-import java.util.concurrent.Executors
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
+import scala.language.{implicitConversions, postfixOps}
 
 
-import scala.annotation.tailrec
-import scala.collection.mutable.ListBuffer
-import scala.language.postfixOps
-import scala.runtime.Nothing$
-import scala.util.control.Breaks.break
-
-
-
-import scala.annotation.tailrec
-
-abstract class LinkedList[+A]{
-  def foreach(f: A => Unit): Unit
-  def head: A
-  def tail: LinkedList[A]
-  def filter(f:A => Boolean) : LinkedList[A]
-  def map[B](f: A=>B) : LinkedList[B]
-  def size : Int
-}
-
-case object InvalidNode extends LinkedList[Nothing] {
-  override def head: Nothing = null.asInstanceOf
-  override def foreach(f: Nothing => Unit): Unit = {}
-  override def tail: LinkedList[Nothing] = null.asInstanceOf
-  override def filter(f: Nothing => Boolean): LinkedList[Nothing] = this
-  override def map[B](f: Nothing => B): LinkedList[B] = this
-  def size = 0
-}
-
-case class ValidNode[A](value: A, next: LinkedList[A]) extends LinkedList[A]
-{
-  override def tail: LinkedList[A] = next
-
-  override def filter(f: A => Boolean): LinkedList[A] = {
-    if (f(value)) ValidNode(value, next.filter(f)) else next.filter(f)
-  }
-  override def map[B]( f : A => B) : LinkedList[B] = {
-    ValidNode(f(value), next.map(f))
-  }
-
-  def size: Int = 1 + next.size
-
-  override def head: A = value
-
-  final override def foreach(f: A => Unit): Unit = {
-    f(value)
-    next.foreach(f)
-  }
-
-}
-
-object LinkedList {
-  def apply[A](args: A*): LinkedList[A] =
-    if (args.isEmpty) InvalidNode
-    else {
-      ValidNode(args.head, apply(args.tail: _*))
-    }
-}
-
-
-object ScalaPracticeDay6 {
-  def main(args : Array[String]): Unit = {
-    val list = LinkedList(1, 2, 3, 4, 5)
-    //list.foreach(println)
-    //println(list.size)
-    //println(list.head)
-    //list.tail.foreach(x => println(x))
-    list.filter(x=>x < 4).foreach(x => println(x))
-    //list.map(x=>x*2).foreach(x => println(x))
-  }
-}
-
-
-
-class Fah(var min: Double, var max: Double, var mean: Double){
+class data(var min: Float, var max: Float, var mean: Float, var count: Int){
   override def toString: String = {
-    s"$min $max $mean"
+    s"$min $max ${mean/count}"
   }
 }
-
-object OBRC {
-  implicit val actorSystem: ActorSystem = ActorSystem("OBRC")
-  implicit val materializer: Materializer = Materializer(actorSystem)
-  implicit val executionContext: ExecutionContextExecutor = ExecutionContext.global
-  //ExecutionContext.fromExecutor(ExecutionContext.fromExecutor(Executors.newFixedThreadPool(64)))
-
-
-  def main(args: Array[String]): Unit = {
-    //val bs : Byte = 1
-    //val map =  mutable.Map[Byte, StringResult]()
-    //val someValue1 = tValue(2,1,2,3)
-    //val someValue2 = tValue(4,1,2,4)
-    //val someValue3 = tValue(1,1,2,5)
-
-    //map(bs) =  StringResult(map.getOrElse(bs,null),someValue1)
-    //map(bs) =  StringResult(map.getOrElse(bs,null),someValue2)
-    //map(bs) =  StringResult(map.getOrElse(bs,null),someValue3)
-
-    //map(bs).foreach(println)
-    //actorSystem.terminate()
-
-    //}
-
-  }
-}
-
 
 object WeatherDataProcessor {
   implicit val actorSystem: ActorSystem = ActorSystem("WeatherDataProcessor")
   implicit val materializer: Materializer = Materializer(actorSystem)
   implicit val executionContext: ExecutionContextExecutor = ExecutionContext.global
 
+  implicit class ByteStringTupleOps(bsTuple: (ByteString, ByteString)) {
+    def asStrings: (String, String) = (bsTuple._1.utf8String, bsTuple._2.utf8String)
+  }
+
+  private def processFrame(bs: ByteString): (ByteString, ByteString) = {
+    var state = true
+    val firstBuilder = ArrayBuffer[Byte]()
+    val secondBuilder = ArrayBuffer[Byte]()
+    bs.foreach { b =>
+      if (b == ';') state = false
+      if (state)
+        firstBuilder += b
+      else
+        secondBuilder += b
+    }
+    val first = ByteString.fromArrayUnsafe(firstBuilder.toArray)
+    val second = ByteString.fromArrayUnsafe(secondBuilder.toArray)
+    (first, second)
+  }
+
+  private def processFrameAsStringBuilder(bs: ByteString): (String, String) = {
+    var state = true
+    val firstBuilder = new StringBuilder()
+    val secondBuilder = new StringBuilder()
+    bs.foreach { b =>
+      if (b == ';') state = false
+      if (state)
+        firstBuilder += b.toChar
+      else
+        secondBuilder += b.toChar
+    }
+    (firstBuilder.toString, secondBuilder.toString)
+  }
+
   def main(args: Array[String]): Unit = {
     val start = System.currentTimeMillis()
-    val result = mutable.Map[String, Fah]()
-    val file = new File(System.getProperty("user.dir") + "/resources/weather_stations.csv")
+    val orderedRes = mutable.HashMap[Byte,mutable.HashMap[String, data]]()
+    val result = mutable.HashMap[String,data]()
+
+    val path = System.getProperty("user.dir") + "\\app\\resources\\weather_stations.csv"
+
+    val file = new File(path)
     val inputStream = new FileInputStream(file)
 
     val source = StreamConverters.fromInputStream(() => inputStream)
       .via(Framing.delimiter(ByteString('\n'), maximumFrameLength = 1024))
-      .map(_.utf8String)
 
-    val sink = Sink.foreach { line =>
-      val l = line.asInstanceOf[String].split(';')
-      println(l(2))
+    val sink = Sink.foreach { bs : ByteString =>
+      val (first,second) = processFrame(bs).asStrings
+      val fah = second.drop(1).toFloat
+      val data: mutable.HashMap[String, data] = orderedRes.getOrElse(first(0).toByte, null)
+      data match {
+        case null =>
 
+          hMap.getOrElse(first,null) match {
+            case value =>
+              if (value.min > fah) value.min = fah
+              if (value.max < fah) value.max = fah
+              value.mean += fah
+            case _ =>
+          }
+
+      }
     }
 
     val stream = source.runWith(sink)
     stream.onComplete { _ =>
-      result.foreach(println)
+      print(orderedRes)
       val end = System.currentTimeMillis()
       println(s"Elapsed time: ${end - start} milliseconds")
       actorSystem.terminate()
     }
   }
 }
+
+
 
